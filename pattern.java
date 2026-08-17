f6c5e882bc0d965de7bd1b13ab1b49b6fa0cd1c3
AyushBytesHQ <aayushadhikari.cool@gmail.com>
2026-08-17 15:51:41 +0545

    add new java code

---
+++ pattern.java
@@ -0,0 +1,18 @@
+// *****
+//  ****
+//   ***
+//    **
+//     *
+public class pattern{
+    public static void main(String[] args){
+        for(int i = 5; i>=1; i--){
+            for(int j = 5; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int l = 1; l<=i; l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
